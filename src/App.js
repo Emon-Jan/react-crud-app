829fc9bff79acf54cd24698f59a669055b5e1c2c
Muktadir Imam Jan <muktadirimam@gmail.com>
2020-10-28 03:00:31 +0600

    react router, antd layout used

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Link, Route } from "react-router-dom";
+
+import { Layout, Menu } from "antd";
+import { UserOutlined, VideoCameraOutlined } from "@ant-design/icons";
+
+import Posts from "./posts/Posts";
+import Categories from "./Categories/Categories";
+
+import "antd/dist/antd.css";
+import "./App.css";
+
+const { Header, Content, Sider } = Layout;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Layout className="layout-wrapper">
+      <Sider breakpoint="lg" collapsedWidth="0">
+        <div className="logo" />
+        <Menu theme="dark" mode="inline" defaultSelectedKeys={["/"]}>
+          <Menu.Item key="/" icon={<UserOutlined />}>
+            <Link to="/">Posts</Link>
+          </Menu.Item>
+          <Menu.Item key="/categories" icon={<VideoCameraOutlined />}>
+            <Link to="/categories">Categories</Link>
+          </Menu.Item>
+        </Menu>
+      </Sider>
+      <Layout>
+        <Header className="site-layout-sub-header-background">CRUD APP</Header>
+        <Content className="site-layout-content">
+          <div className="site-layout-background">
+            <Route path="/" component={Posts} exact={true} />
+            <Route path="/categories" component={Categories} />
+          </div>
+        </Content>
+      </Layout>
+    </Layout>
   );
 }
 
