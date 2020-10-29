882c1b64db5ebbdb2e00079735243bec677e023b
Muktadir Imam Jan <muktadirimam@gmail.com>
2020-10-30 01:50:49 +0600

    post updated

--- src/posts/post/Post.js
+++ src/posts/post/Post.js
@@ -1,21 +1,45 @@
 import React from "react";
+import { Card, Tag, Row, Col } from "antd";
+import { EditOutlined, DeleteOutlined } from "@ant-design/icons";
 import "./Post.css";
-import { Card } from "antd";
-import { EditOutlined, EllipsisOutlined } from "@ant-design/icons";
 
 const { Meta } = Card;
 
 function Post(props) {
+  const tags = props.post.category.map((t, index) => (
+    <Tag className="tag-name" key={index}>
+      {t}
+    </Tag>
+  ));
+
   return (
     <Card
       className="card-post"
       hoverable={true}
       actions={[
-        <EditOutlined key="edit" onClick={() => console.log("clicked")} />,
-        <EllipsisOutlined key="ellipsis" />,
+        <EditOutlined
+          key="edit"
+          onClick={() => props.onEditPost(props.index)}
+        />,
+        <DeleteOutlined
+          key="delete"
+          onClick={() => {
+            props.onDeletePost(props.index);
+          }}
+        />,
       ]}
     >
-      <Meta title={props.post.title} description={props.post.description} />
+      <Row gutter={16}>
+        <Col span={16}>
+          <Meta title={props.post.title} description={props.post.description} />
+        </Col>
+        <Col span={4}>
+          <Card bordered={false}>
+            <Meta title="Category" style={{ padding: "0 0 5px 0" }}></Meta>
+            {tags}
+          </Card>
+        </Col>
+      </Row>
     </Card>
   );
 }
