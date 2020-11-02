63c89d5fd9f9ff3477ddea0bf9446faacb2cf4f3
Muktadir Imam Jan <muktadirimam@gmail.com>
2020-11-02 21:19:25 +0600

    refactored

--- src/App.js
+++ src/App.js
@@ -18,37 +18,20 @@ function App(props) {
   const [posts, setPosts] = useState([
     {
       title: "Post 1",
-      description:
-        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled",
+      description: "Lorem ",
       category: ["p1", "p2", "p3"],
     },
     {
       title: "Post 2",
-      description:
-        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled",
+      description: "Lorem Ipsum ",
       category: ["p3", "p5"],
     },
-    {
-      title: "Post 3",
-      description:
-        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled",
-      category: ["p4", "p7"],
-    },
   ]);
 
-  const [categories, setCategories] = useState([
-    "p1",
-    "p2",
-    "p3",
-    "p4",
-    "p5",
-    "p6",
-    "p7",
-    "p8",
-  ]);
+  const [categories, setCategories] = useState(["p1", "p2", "p3"]);
 
   const childProps = {
-    showPosts: posts,
+    posts: posts,
     categories,
     setCategories,
     setShowPost: setPosts,
